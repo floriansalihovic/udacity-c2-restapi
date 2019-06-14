0d778225a2bf219ac182e1d933d537276f8f8269
Florian Salihovic <florian.salihovic@me.com>
2019-06-14 20:41:45 +0200

    using environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE_NAME,
+    "host": process.env.POSTGRES_HOST_NAME,
     "dialect": "postgres",
-    "aws_reigion": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE_NAME,
+    "aws_media_bucket": process.env.AWS_BUCKET_NAME
   },
   "prod": {
     "username": "",
